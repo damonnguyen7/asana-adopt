6ae03abc0c965db0e61e9023d846c7c6b2ded198
Damon <damonnguyen92@gmail.com>
2019-08-28 13:17:05 -0700

    Add compression middleware for compressing responses.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const compression = require('compression')
 
 const appRouter = require('./routers/appRouter');
 const photoRouter = require('./routers/photoRouter');
@@ -17,6 +18,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', appRouter);
