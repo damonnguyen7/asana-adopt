276793656eafd8ebac1da09c68a4d03328217634
Damon <damonnguyen92@gmail.com>
2019-08-28 13:23:13 -0700

    Cleaned up codebase and fix bugs.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,8 +1,9 @@
-const container = document.getElementById("container");
 const overlay = document.querySelector('#overlay');
 const body = document.getElementsByTagName('body')[0];
 const paginationBtnContainer = document.querySelector('.pagination__btn-container');
+const { fetchDogs, createCard, renderGridList, renderPagination } = require('./utils');
 const url = '../assets/data/dogs.json';
+
 let state = {
   dogs: [],
   pagination: {
@@ -13,19 +14,21 @@ let state = {
   }
 };
 
-async function asyncRenderGridList() {
+async function asyncRenderGridListAndPagination() {
   const dogs = await fetchDogs(url);
+  const totalPages = Math.ceil(dogs.length / 12);
   state = {
     dogs,
     pagination: {
       currentPage: 1,
-      totalPages: Math.ceil(dogs.length / 12),
-      start: undefined,
-      end: undefined
+      totalPages,
+      start: 1,
+      end: totalPages <= 7 ? totalPages : 7
     }
-  }
+  };
+
   renderGridList(dogs.slice(0, 12));
-  renderPagination(1,  state.pagination.totalPages <= 7 ? state.pagination.totalPages : 7);
+  renderPagination(1,  totalPages <= 7 ? totalPages : 7, 1);
 }
 
 overlay.addEventListener('click', (event) => {
@@ -34,21 +37,26 @@ overlay.addEventListener('click', (event) => {
 });
 
 paginationBtnContainer.addEventListener('click', (e) => {
-  const { dogs, pagination } = state;
-  const { currentPage, totalPages, start, end } = pagination;
+  const dogs = state.dogs;
   const paginationBtn = event.target.closest('.pagination__btn');
-  const previousPage = currentPage;
-  state.pagination.currentPage = Number(paginationBtn.dataset.id);
-  if (currentPage <= totalPages) {
-    if (currentPage < 7) {
-      renderPagination(1,  totalPages <= 7 ? totalPages : 7);
-    } else {
-      state.pagination.start = currentPage - 3;
-      state.pagination.end = currentPage + 3 <= totalPages ? currentPage + 3 : totalPages;
-      renderPagination(start,  end);
+  const clickedPaginationBtn = paginationBtn !== null;
+  const hasNotBeenClicked = state.pagination.currentPage !== paginationBtn.dataset.id;
+
+  if (clickedPaginationBtn && hasNotBeenClicked) {
+    window.scrollTo({top: 0});
+    state.pagination.currentPage = Number(paginationBtn.dataset.id);
+    if (state.pagination.currentPage <= state.pagination.totalPages) {
+      if (state.pagination.currentPage < 7) {
+        renderPagination(1,  state.pagination.totalPages <= 7 ? state.pagination.totalPages : 7, state.pagination.currentPage);
+      } else {
+        state.pagination.start = state.pagination.currentPage - 3;
+        state.pagination.end = state.pagination.currentPage + 3 <= state.pagination.totalPages ? state.pagination.currentPage + 3 : state.pagination.totalPages;
+        renderPagination(state.pagination.start,  state.pagination.end, state.pagination.currentPage);
+      }
     }
+
+    renderGridList(dogs.slice(state.pagination.currentPage * 12 - 12, state.pagination.currentPage * 12));
   }
-  renderGridList(dogs.slice(state.pagination.currentPage * 12 - 12, state.pagination.currentPage * 12));
 });
 
-asyncRenderGridList();
+asyncRenderGridListAndPagination();
