55669481fe8e9b13ec32ced08fcd79957a33aa78
Damon <damonnguyen92@gmail.com>
2019-08-28 09:02:44 -0700

    Delete apiRouter.

--- routers/apiRouter.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/dogs', function(req, res, next) {
-  res.render('index', { title: 'Asana Adopt' });
-});
-
-module.exports = router;
