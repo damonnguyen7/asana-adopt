ead489f61cb3f73485aadc749b8a9afb0ec86558
Damon <damonnguyen92@gmail.com>
2019-08-29 12:04:30 -0700

    Update document title.

--- routers/appRouter.js
+++ routers/appRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Asana Adopt' });
+  res.render('index', { title: 'Asana Adopt - Adopt a best friend' });
 });
 
 module.exports = router;
